09cf32eb7c9ab546f0e4fb7cc688d04c59c3b540
firsaev <fir555@gmail.com>
2020-01-08 19:20:10 +0300

    Updates

---
+++ precache-manifest.061ab4a4d10ccf468007d2ad99e50fe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6dd53818ddd7247d1145181914c73a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b322ea56dee46500fa8",
+    "url": "/static/js/2.45faa6e7.chunk.js"
+  },
+  {
+    "revision": "16a9b33bee9a2dbe384e4db01f592549",
+    "url": "/static/js/2.45faa6e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eda48eff7e7fbc00ce52",
+    "url": "/static/js/main.a894823e.chunk.js"
+  },
+  {
+    "revision": "837a1cd991c007553491",
+    "url": "/static/js/runtime-main.2ac9737b.js"
+  }
+]);
